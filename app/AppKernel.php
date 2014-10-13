3b4dc592df3d079b3a6db6dc753a9c7c66218170
Reinier Kip <reinier@ibuildings.nl>
2014-10-13 14:30:55 +0200

    Install and configure JMSTranslationBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,6 +39,9 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new JMS\TranslationBundle\JMSTranslationBundle();
+            $bundles[] = new JMS\DiExtraBundle\JMSDiExtraBundle($this);
+            $bundles[] = new JMS\AopBundle\JMSAopBundle();
         }
 
         return $bundles;
