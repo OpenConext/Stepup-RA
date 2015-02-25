51bb6ed37717d634fe7543ac9a29d72cc588b50a
Reinier Kip <reinier@ibuildings.nl>
2015-02-25 16:41:32 +0100

    List second factors.

---
+++ src/Controller/SecondFactorController.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupRa\RaBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Surfnet\StepupMiddlewareClient\Identity\Dto\RaSecondFactorSearchQuery;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+final class SecondFactorController extends Controller
+{
+    /**
+     * @Template
+     */
+    public function searchAction()
+    {
+        $secondFactors = $this->get('ra.service.ra_second_factor')->search(
+            new RaSecondFactorSearchQuery('Ibuildings bv')
+        );
+
+        return [
+            'secondFactors' => $secondFactors,
+        ];
+    }
+}
