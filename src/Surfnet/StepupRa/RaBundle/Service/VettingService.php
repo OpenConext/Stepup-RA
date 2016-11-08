6c581a343fa251723960914f6cff402f5ebd5f72
Alex Rothuis <arothuis@ibuildings.nl>
2016-11-08 11:56:41 +0100

    Map MW-specific error message to constant
    This way, we can determine whether this error occurs and it enables us to translate it to an RA-specific message.

--- src/Surfnet/StepupRa/RaBundle/Service/VettingService.php
+++ src/Surfnet/StepupRa/RaBundle/Service/VettingService.php
@@ -50,6 +50,9 @@ use Symfony\Component\Translation\TranslatorInterface;
  */
 class VettingService
 {
+    const REGISTRATION_CODE_EXPIRED_ERROR =
+        'Surfnet\Stepup\Exception\DomainException: Cannot vet second factor, the registration window is closed.';
+
     /**
      * @var \Surfnet\StepupBundle\Service\SmsSecondFactorService
      */
