e7eb5135bd1dc3a6543f4e506adf83e6e8671ef0
Michiel Kodde <mkodde@ibuildings.nl>
2017-06-01 13:23:53 +0200

    Code cleanup

--- src/Surfnet/StepupRa/RaBundle/DependencyInjection/Configuration.php
+++ src/Surfnet/StepupRa/RaBundle/DependencyInjection/Configuration.php
@@ -18,9 +18,6 @@
 
 namespace Surfnet\StepupRa\RaBundle\DependencyInjection;
 
-use Surfnet\StepupBundle\Exception\DomainException;
-use Surfnet\StepupBundle\Exception\InvalidArgumentException;
-use Surfnet\StepupBundle\Value\SecondFactorType;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
--- src/Surfnet/StepupRa/SamlStepupProviderBundle/DependencyInjection/Configuration.php
+++ src/Surfnet/StepupRa/SamlStepupProviderBundle/DependencyInjection/Configuration.php
@@ -22,6 +22,9 @@ use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
+/**
+ * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+ */
 class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
