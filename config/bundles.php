464ffb25166ede0292b0c3dccc79b111687cb880
Johan Kromhout <jkromhout@ibuildings.nl>
2026-01-13 14:51:29 +0100

    Update Symfony 6.4 > 7.4

--- config/bundles.php
+++ config/bundles.php
@@ -9,7 +9,6 @@ return [
     OpenConext\MonitorBundle\OpenConextMonitorBundle::class => ['all' => true],
     Surfnet\SamlBundle\SurfnetSamlBundle::class => ['all' => true],
     Surfnet\StepupBundle\SurfnetStepupBundle::class => ['all' => true],
-    Surfnet\StepupMiddlewareClientBundle\SurfnetStepupMiddlewareClientBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Surfnet\StepupRa\RaBundle\SurfnetStepupRaRaBundle::class => ['all' => true],
     Surfnet\StepupRa\SamlStepupProviderBundle\SurfnetStepupRaSamlStepupProviderBundle::class => ['all' => true],
@@ -18,4 +17,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'smoketest' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Surfnet\StepupMiddlewareClientBundle\SurfnetStepupMiddlewareClientBundle::class => ['all' => true],
 ];
